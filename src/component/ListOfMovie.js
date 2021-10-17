70638b181faf0a64f7e50fdf0dee5531658b2c23
shree <sristikumari1996@gmail.com>
2021-10-17 14:09:30 +0530

    bootstarp is not working

---
+++ src/component/ListOfMovie.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ListOfMovie = (props) => {
+	const FavouriteComponent = props.favouriteComponent;
+
+    if(props.movies) {
+        return (
+            <>
+                {props.movies.map((movie, index) => (
+                    <div className='image-container d-flex justify-content-start m-3'>
+                        <img src={movie.Poster} alt='movie'></img>
+                        <div
+                            onClick={() => props.handleFavouritesClick(movie)}
+                            className='overlay d-flex align-items-center justify-content-center'
+                        >
+                            <FavouriteComponent />
+                        </div>
+                    </div>
+                ))}
+            </>
+        );
+    }
+    else {
+        return <></>;
+    }
+};
+
+
+export default ListOfMovie;
